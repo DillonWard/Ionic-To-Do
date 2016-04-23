f4421986fdcfe41294ed449883ce5fdbd6183210
Dillon Ward <Dillon Ward>
2016-04-23 23:29:02 +0100

    Comments added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,22 +2,25 @@ angular.module('starter.controllers', [])
 
 .controller('ListsCtrl', function($scope, Task) {
     $scope.tasks = Task.data;    
-   
+   // Removes item from list of tasks
     $scope.remove = function(item){
         Task.remove(item);
     };
     
     var data = {
+        // Variables for new tasks to add
         newTask: "",
         newDetail: ""
     };
     
 
     function addTask(){
-                
+            
+        // If it's empty it doesn't add to list if empty
         if($scope.data.newTask === ""){
         }
         
+        // Else it's added
         else{
           Task.addTask(data.newTask, data.newDetail)
           
@@ -39,6 +42,7 @@ angular.module('starter.controllers', [])
         newDetail: ""  
     };
     
+    // Editing tasks
     function editTask(){
         console.log($scope.data);
         Task.editTask($stateParams.taskID, $scope.data.newName, $scope.data.newDetail);
--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ angular.module('starter.services', [])
 .factory('Task', function() {
   
   var data = {  
-      
+      // Sample Tasks
       tasks: [{
           
           id: 0,
@@ -15,29 +15,33 @@ angular.module('starter.services', [])
     ] 
   }
   
-  var ids;
-
-    function addTask(title, detail) {
-    data.tasks.push({id: data.tasks.length + 1, title: title, detail: detail, added: new Date()});
+  
+  // Adding a task to the list
+  // Adds title, details and date
+  function addTask(title, detail) {
+      data.tasks.push({id: data.tasks.length + 1, title: title, 
+                       detail: detail, added: new Date()});
+  
   }
   
+    // sets IDs to tasks
     function getter(id){
-        
         for(var i = 0; i <data.tasks.length; i++){
             if(data.tasks[i].id === parseInt(id)){
-                return data.tasks[i];
+                return data.tasks[i]; 
             }
             
         }
         return null;
     }
-    
+    // Function for removing tasks
     function remove(id){
       data.tasks.splice(data.tasks.indexOf(id), 1);
   }
-    
+    // Function for editing task
     function editTask(id, newName, newDetail){
-        
+        // Loops through the list of tasks
+        // Changes the task with the same ID through new entries
         for(var i = 0; i < data.tasks.length; i++){
             if(data.tasks[i].id === parseInt(id)){
                 data.tasks[i].title = newName;
